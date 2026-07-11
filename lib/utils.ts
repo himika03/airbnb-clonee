1b218b3bbc160712f2c4de47cd91724ac83860a7
Himika Kakhani <himikakakhani.cs23@bmsce.ac.in>
2026-07-11 16:37:17 +0530

    Change currency to INR

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(price: number): string {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(price);
